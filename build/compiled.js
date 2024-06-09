0e59894b6c29a8eef7f421d7d4d356ad49a8372f
Bill Harding <wbharding@gmail.com>
2024-06-08 17:34:04 -0700

    Update compiled code

---
+++ build/compiled.js
@@ -0,0 +1,111 @@
+(() => {
+  // lib/plugin.js
+  var plugin = {
+    // --------------------------------------------------------------------------------------
+    constants: {
+      DEFAULT_NOTE_DATA_NAME: "LifeTracker Data",
+      DEFAULT_NOTE_DATA_TAGS: ["data"],
+      NOTE_DATA_SETTING_KEY: "Note Data Name"
+    },
+    // --------------------------------------------------------------------------------------
+    appOption: {
+      "Record mood level": async function(app) {
+        const noteDataName = await this._fetchNoteDataName(app);
+        const moodOptions = [1, 2, 3, 4, 5].map((value) => ({ value, label: value }));
+        const result = await app.prompt("Record your current state", { inputs: [
+          { label: "Mood level", type: "radio", options: moodOptions },
+          { label: "What happened in the time leading up to this rating?", type: "text" }
+        ] });
+        const lifeDataNote = await this._fetchData(app, noteDataName);
+        await this._persistData(app, lifeDataNote, "Mood level", result);
+      }
+    },
+    // --------------------------------------------------------------------------------------
+    _persistData: async function(app, note, sectionName, result) {
+      const content = await app.getNoteSections(note);
+      let existingTable = "";
+      if (content.includes(`# ${sectionName}`)) {
+        existingTable = await this._sectionContent(content, sectionName);
+        if (existingTable?.length) {
+          existingTable = existingTable.split("\n").slice(2).join("\n");
+        }
+      } else {
+        await note.insertContent(`# ${sectionName}
+`);
+      }
+      let tableMarkdown = `| Date | Mood | Precipitating events |
+| --- | --- | --- |
+`;
+      tableMarkDown += `| ${(/* @__PURE__ */ new Date()).toISOString()} | ${result[0]} | ${result[1]} |
+`;
+      tableMarkdown += existingTable;
+      await note.replaceNoteContent(tableMarkdown, { heading: { text: sectionName, level: 2 } });
+    },
+    // --------------------------------------------------------------------------------------
+    _fetchData: async function(app, noteDataName) {
+      const existingNote = await app.findNote({ name: noteDataName });
+      if (existingNote) {
+        return existingNote;
+      }
+      const note = await app.createNote(noteDataName, this.constants.DEFAULT_NOTE_DATA_TAGS);
+      return note;
+    },
+    // --------------------------------------------------------------------------------------
+    _fetchNoteDataName: async function(app) {
+      let noteDataName = await app.settings[this.constants.NOTE_DATA_SETTING_KEY];
+      if (!noteDataName) {
+        const result = await app.prompt(
+          `Enter the name of the note in which you'd like to record life data (default is "${this.constants.DEFAULT_NOTE_DATA_NAME}")`,
+          { inputs: [{ type: "text" }] }
+        );
+        const noteName = result[0] || this.constants.DEFAULT_NOTE_DATA_NAME;
+        noteDataName = noteName;
+        await app.setSetting(this.constants.NOTE_DATA_SETTING_KEY, noteDataName);
+      }
+      return noteDataName;
+    },
+    // --------------------------------------------------------------------------------------
+    // Return all of the markdown within a section that begins with `sectionHeadingText`
+    // `sectionHeadingText` Text of the section heading to grab, with or without preceding `#`s
+    // `depth` Capture all content at this depth, e.g., if grabbing depth 2 of a second-level heading, this will return all potential h3s that occur up until the next h1 or h2
+    _sectionContent(noteContent, headingTextOrSectionObject) {
+      console.debug(`_sectionContent()`);
+      let sectionHeadingText;
+      if (typeof headingTextOrSectionObject === "string") {
+        sectionHeadingText = headingTextOrSectionObject;
+      } else {
+        sectionHeadingText = headingTextOrSectionObject.heading.text;
+      }
+      try {
+        sectionHeadingText = sectionHeadingText.replace(/^#+\s*/, "");
+      } catch (err) {
+        if (err.name === "TypeError") {
+          throw new Error(`${err.message} (line 1054)`);
+        }
+      }
+      const { startIndex, endIndex } = _sectionRange(noteContent, sectionHeadingText);
+      return noteContent.slice(startIndex, endIndex);
+    },
+    // --------------------------------------------------------------------------------------
+    // Return {startIndex, endIndex} where startIndex is the index at which the content of a section
+    // starts, and endIndex the index at which it ends.
+    _sectionRange(bodyContent, sectionHeadingText) {
+      console.debug(`_sectionRange`);
+      const sectionRegex = /^#+\s*([^#\n\r]+)/gm;
+      const indexes = Array.from(bodyContent.matchAll(sectionRegex));
+      const sectionMatch = indexes.find((m) => m[1].trim() === sectionHeadingText.trim());
+      if (!sectionMatch) {
+        console.error("Could not find section", sectionHeadingText, "that was looked up. This might be expected");
+        return { startIndex: null, endIndex: null };
+      } else {
+        const level = sectionMatch[0].match(/^#+/)[0].length;
+        const nextMatch = indexes.find((m) => m.index > sectionMatch.index && m[0].match(/^#+/)[0].length <= level);
+        const endIndex = nextMatch ? nextMatch.index : bodyContent.length;
+        return { startIndex: sectionMatch.index + sectionMatch[0].length + 1, endIndex };
+      }
+    }
+    // There are several other entry points available, check them out here: https://www.amplenote.com/help/developing_amplenote_plugins#Actions
+    // You can delete any of the insertText/noteOptions/replaceText keys if you don't need them
+  };
+  var plugin_default = plugin;
+})();
